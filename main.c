29417ddb1512ecf2694cfeda9bc6705f61d2bb75
ben256dev <benjamin@ben256.com>
2026-02-21 19:14:58 -0700

    constrain leg length

--- main.c
+++ main.c
@@ -1,36 +1,45 @@
 #include <raylib.h>
+#include <math.h>
 
-#define SCREEN_WIDTH 1280
-#define SCREEN_HEIGHT 720
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 576
 #define UPDATE_DT (1.0f / 60.0f)
+#define MAGNITUDE(vec) sqrtf(vec.x * vec.x + vec.y * vec.y)
 
 int main(void) {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Bird Demo");
     SetTargetFPS(60);
 
-    Vector2 player_pos = { SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f };
-    Vector2 velocity = { 180.0f, 140.0f };
-    float radius = 22.0f;
+    float velocity = 0.0f;
+    float radius = 30.0f;
+    float target_radius = 15.0f;
+    float player_speed = 400.0f;
+    float player_height = 140.0f;
+    float floor_height = 60.0f;
+    float leg_thickness = 8.0f;
+
+    Vector2 player_pos = { SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT - player_height - floor_height };
+    Vector2 foot_target = { player_pos.x, player_pos.y + player_height };
 
     while (!WindowShouldClose()) {
-        player_pos.x += velocity.x * UPDATE_DT;
-        player_pos.y += velocity.y * UPDATE_DT;
-
-        if (player_pos.x - radius <= 0.0f || player_pos.x + radius >= SCREEN_WIDTH) {
-            velocity.x = -velocity.x;
-        }
-        if (player_pos.y - radius <= 0.0f || player_pos.y + radius >= SCREEN_HEIGHT) {
-            velocity.y = -velocity.y;
-        }
+        int left_right = (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) - (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT));
+        velocity = left_right * player_speed;;
+        player_pos.x += velocity * UPDATE_DT;
 
         BeginDrawing();
         ClearBackground(BLACK);
 
-        DrawCircleV(player_pos, radius, WHITE);
-        DrawCircleLines(player_pos.x, player_pos.y, radius + 6.0f, WHITE);
+        Vector2 foot_vec_constrained = { foot_target.x - player_pos.x, foot_target.y - player_pos.y };
+        float unconstrained_distance = MAGNITUDE(foot_vec_constrained);
+        if (unconstrained_distance > player_height) {
+            foot_vec_constrained.x *= player_height / unconstrained_distance;
+            foot_vec_constrained.y *= player_height / unconstrained_distance;
+        }
+        Vector2 foot_pos_constrained = { player_pos.x + foot_vec_constrained.x, player_pos.y + foot_vec_constrained.y };
 
-        DrawText("raylib 2D example", 20, 20, 20, WHITE);
-        DrawText("ESC to quit", 20, 48, 16, WHITE);
+        DrawCircleV(player_pos, radius, WHITE);
+        DrawLineEx(player_pos, foot_pos_constrained, leg_thickness, WHITE);
+        DrawCircleLinesV(foot_target, target_radius, YELLOW);
 
         EndDrawing();
     }
