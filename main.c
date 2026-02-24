d7645068a25db7fa3e6a4500e0e94544d3bed0ea
ben256dev <benjamin@ben256.com>
2026-02-23 20:25:36 -0700

    add acceleration and step target

--- main.c
+++ main.c
@@ -6,6 +6,17 @@
 #define UPDATE_DT (1.0f / 60.0f)
 #define MAGNITUDE(vec) sqrtf(vec.x * vec.x + vec.y * vec.y)
 
+float fsign(float x) {
+    if (x > 0.0f) {
+        return 1.0f;
+    } else if (x < 0.0f) {
+        return -1.0f;
+    } else {
+        return 0.0f;
+    }
+}
+
+
 Vector2 two_form_solve_elbow_vec(Vector2 shoulder, Vector2 end_effector, float l1, float l2, int elbow_direction_sign) {
     float x = end_effector.x - shoulder.x;
     float y = end_effector.y - shoulder.y;
@@ -32,18 +43,29 @@ int main(void) {
     float radius = 30.0f;
     float target_radius = 15.0f;
     float player_speed = 400.0f;
+    float player_acceleration = 10.0f;
     float player_height = 140.0f;
     float floor_height = 60.0f;
     float leg_thickness = 8.0f;
     float leg_length = 160.0f;
 
+    float stride_length = 150.0f;
+
     Vector2 player_pos = { SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT - player_height - floor_height };
     Vector2 feet[2] = { { player_pos.x + 40, player_pos.y + player_height }, { player_pos.x, player_pos.y + player_height } };
+    Vector2 step_target = { player_pos.x + velocity / player_speed * stride_length / 2.0f, player_pos.y + player_height };
 
     while (!WindowShouldClose()) {
         int left_right = (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) - (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT));
-        velocity = left_right * player_speed;;
+
+        velocity += left_right * player_acceleration;;
+        if (fabsf(velocity) > player_speed) {
+            velocity = fsign(velocity) * player_speed;
+        }
+
         player_pos.x += velocity * UPDATE_DT;
+
+        step_target.x = player_pos.x + velocity / player_speed * stride_length / 2.0f;
 
         BeginDrawing();
         ClearBackground(BLACK);
@@ -63,8 +85,10 @@ int main(void) {
             Vector2 elbow_pos = { player_pos.x + elbow_vec.x, player_pos.y + elbow_vec.y };
             DrawLineEx(player_pos, elbow_pos, leg_thickness, WHITE);
             DrawLineEx(elbow_pos, foot_pos_constrained, leg_thickness, WHITE);
-            DrawCircleLinesV(feet[i], target_radius, YELLOW);
+            DrawCircleLinesV(feet[i], target_radius, WHITE);
         }
+
+        DrawCircleV(step_target, target_radius, YELLOW);
 
         EndDrawing();
     }
